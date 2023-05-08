8d80ce98ebdbbf74aec1d0f694a7c06de139a56f
Marma92 <elbaz.amram@gmail.com>
2023-05-08 15:44:44 +0200

    update the soon gui of our app

--- libs/sppgui.py
+++ libs/sppgui.py
@@ -1,27 +1,44 @@
 import tkinter as tk
 from tkinter import filedialog
 import requests
+from PIL import Image, ImageTk
 
 # Event handler for the "Upload" button
 def upload_file():
-    file_path = filedialog.askopenfilename()  # Open a file dialog to select a file
+    file_path = filedialog.askopenfilename(filetypes=[("JPEG files", "*.jpg;*.jpeg")])  # Open a file dialog to select a file
     # Implement file upload logic using the selected file path
-    # For example, you can use the 'requests' library to make a POST request with the file
-    with open(file_path, 'rb') as file:
-        response = requests.post('http://example.com/upload', files={'file': file})
-        print(response.text)  # Print the response from the server
+
+    # Display a preview of the selected JPEG image
+    image = Image.open(file_path)
+    image.thumbnail((320, 320))  # Resize the image to fit within a 320x320 pixel box
+    photo = ImageTk.PhotoImage(image)
+    image_label.config(image=photo)
+    image_label.image = photo
+
+    # Set the size and position of the image label
+    image_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
 # Create the main window
 root = tk.Tk()
 root.title("File Upload")
 
-# Add a label
-label = tk.Label(root, text="Select a file to upload:")
-label.pack(pady=10)
+# Set the dimensions of the window to 640x480 pixels
+root.geometry("640x480")
+
+# Add a label to display the preview image
+image = Image.new('RGB', (320, 320), 'white')  # Create a white image with the same size as the preview image
+photo = ImageTk.PhotoImage(image)
+image_label = tk.Label(root, image=photo)
+image_label.image = photo
+image_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
 # Add an "Upload" button
 upload_button = tk.Button(root, text="Upload", command=upload_file)
-upload_button.pack()
+upload_button.grid(row=1, column=0, pady=10, sticky='n', padx=10)
+
+# Add a blank label to center the button
+blank_label = tk.Label(root)
+blank_label.grid(row=2, column=0)
 
 # Start the Tkinter event loop
 root.mainloop()
