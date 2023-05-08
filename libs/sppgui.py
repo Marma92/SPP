a906cf52dde9752c715bc022fac15beafcbef533
ELBAZ Amram <elbaz.amram@gmail.com>
2023-05-08 20:36:52 +0200

    GUI gets its fields

--- libs/sppgui.py
+++ libs/sppgui.py
@@ -19,20 +19,36 @@ def upload_file():
 
 # Event handler for the "Submit" button
 def submit_form():
-    # Get the values of the title and description fields
+    # Get the values of the form fields
     title_value = title_field.get()
     description_value = description_field.get()
+    hashtags_value = hashtags_field.get()
+    camera_value = camera_field.get()
+    lens_value = lens_field.get()
+    film_value = film_field.get()
+    lab_value = lab_field.get()
+    scan_value = scan_field.get()
+    date_value = date_field.get()
+    location_value = location_field.get()
 
     # Do something with the form data, e.g. send it to a server or save it to a file
     print("Title:", title_value)
     print("Description:", description_value)
+    print("Hashtags:", hashtags_value)
+    print("Camera:", camera_value)
+    print("Lens:", lens_value)
+    print("Film:", film_value)
+    print("Lab:", lab_value)
+    print("Scan:", scan_value)
+    print("Date:", date_value)
+    print("Location:", location_value)
 
 # Create the main window
 root = tk.Tk()
 root.title("File Upload")
 
-# Set the dimensions of the window to 800x480 pixels
-root.geometry("800x480")
+# Set the dimensions of the window to 1000x480 pixels
+root.geometry("640x480")
 
 # Add a label to display the preview image
 image = Image.new('RGB', (320, 320), 'white')  # Create a white image with the same size as the preview image
@@ -61,9 +77,57 @@ description_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
 description_field = tk.Entry(form_frame)
 description_field.grid(row=1, column=1, padx=5, pady=5, sticky='w')
 
+# Add a label and entry field for the hashtags
+hashtags_label = tk.Label(form_frame, text="Hashtags:")
+hashtags_label.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+hashtags_field = tk.Entry(form_frame)
+hashtags_field.grid(row=2, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the camera
+camera_label = tk.Label(form_frame, text="Camera:")
+camera_label.grid(row=3, column=0, padx=5, pady=5, sticky='w')
+camera_field = tk.Entry(form_frame)
+camera_field.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the lens
+lens_label = tk.Label(form_frame, text="Lens:")
+lens_label.grid(row=4, column=0, padx=5, pady=5, sticky='w')
+lens_field = tk.Entry(form_frame)
+lens_field.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the film
+film_label = tk.Label(form_frame, text="Film:")
+film_label.grid(row=5, column=0, padx=5, pady=5, sticky='w')
+film_field = tk.Entry(form_frame)
+film_field.grid(row=5, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the film lab
+lab_label = tk.Label(form_frame, text="Film lab:")
+lab_label.grid(row=6, column=0, padx=5, pady=5, sticky='w')
+lab_field = tk.Entry(form_frame)
+lab_field.grid(row=6, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the scan
+scan_label = tk.Label(form_frame, text="Scanning:")
+scan_label.grid(row=7, column=0, padx=5, pady=5, sticky='w')
+scan_field = tk.Entry(form_frame)
+scan_field.grid(row=7, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the date
+date_label = tk.Label(form_frame, text="Date:")
+date_label.grid(row=8, column=0, padx=5, pady=5, sticky='w')
+date_field = tk.Entry(form_frame)
+date_field.grid(row=8, column=1, padx=5, pady=5, sticky='w')
+
+# Add a label and entry field for the location
+location_label = tk.Label(form_frame, text="Location name:")
+location_label.grid(row=9, column=0, padx=5, pady=5, sticky='w')
+location_field = tk.Entry(form_frame)
+location_field.grid(row=9, column=1, padx=5, pady=5, sticky='w')
+
 # Add a "Submit" button
-submit_button = tk.Button(form_frame, text="Post", command=submit_form)
-submit_button.grid(row=2, column=1, padx=5, pady=5, sticky='e')
+submit_button = tk.Button(form_frame, text="Submit", command=submit_form)
+submit_button.grid(row=12, column=1, padx=5, pady=5, sticky='e')
 
 # Start the Tkinter event loop
 root.mainloop()
